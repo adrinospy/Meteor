e20c48e70e32dee21e509000d970bb4b0e51afef
adrinospy <amanmoon099@gmail.com>
2024-02-03 05:28:53 +0530

    changed the logo colour

--- src/app/_component/logo.tsx
+++ src/app/_component/logo.tsx
@@ -2,7 +2,7 @@ import { DiMeteorfull } from "react-icons/di";
 
 export default function Logo() {
     return (<DiMeteorfull
-        style={{ color: 'black', height: '80px', width: "220px" }}
+        style={{ color: 'white', height: '80px', width: "220px" }}
         viewBox="8.5 12 10 10"
     />)
 }
