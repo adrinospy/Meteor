93aa0c2d12704db61f2695fa06584f24fe645c79
adrinospy <amanmoon099@gmail.com>
2024-02-03 02:08:02 +0530

    created logo component

---
+++ src/app/_component/logo.tsx
@@ -0,0 +1,8 @@
+import { DiMeteorfull } from "react-icons/di";
+
+export default function Logo() {
+    return (<DiMeteorfull
+        style={{ color: 'black', height: '80px', width: "220px" }}
+        viewBox="8.5 12 10 10"
+    />)
+}
