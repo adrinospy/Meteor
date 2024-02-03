9fee5f8093b35b08b7a2d0c0046a538fb5d376c4
adrinospy <amanmoon099@gmail.com>
2024-02-03 05:30:26 +0530

    created the navbar component

---
+++ src/app/_component/nav.tsx
@@ -0,0 +1,28 @@
+import Logo from "./logo"
+import Link from "next/link"
+
+import FlippableButton from "../ui/button/Flipping_button"
+
+const NavLinks = () => {
+
+    return (
+        <>
+        <Link className="px-[35px] font-semibold" href="/">Home</Link>
+        <Link className="px-[35px] font-semibold" href="/features">Features</Link>
+        <Link className="px-[35px] font-semibold" href="/about">About</Link>
+        <Link className="pl-[35px] pr-[80px] font-semibold" href="/projects">Projects</Link>
+        </>
+    )
+    }
+    
+    export default function NavBar() {
+        
+        return (
+        <header className="pt-[20px] mx-auto max-w-[1440px] flex justify-between">
+            <div className="justify-self-start"><Logo/></div>
+                <div className="flex items-center"><NavLinks />
+            <FlippableButton BfrTxt="Start a Meet?" AftTxt="Signin / login" />  
+                </div>
+        </header>
+    )
+}
