1c9ac39d98a46ca5c2431ba5bdee8b70683db281
adrinospy <amanmoon099@gmail.com>
2024-03-13 23:42:24 +0530

    a

--- .github/workflows/add-labels.sh
+++ .github/workflows/add-labels.sh
@@ -11,51 +11,55 @@
 #   MODERATE_THRESHOLD
 #   LARGE_THRESHOLD
 
-git fetch origin $GITHUB_BASE_REF
-CHANGES=$(git diff --ignore-all-space $PR_BASE_SHA $PR_HEAD_SHA)
+size_label() {
 
-AUTH_HEADER="Authorization: Bearer $GITHUB_TOKEN"
+    local CHANGES=$1
+    local arg2=$2
+    AUTH_HEADER="Authorization: Bearer $GITHUB_TOKEN"
 
-# get all the changes from git diff
-CHANGES=$(echo "$CHANGES" | grep -E '^[+\-]' | grep -vE '^\+\+\+|^\-\-\-')
+    # get all the changes from git diff
+    CHANGES=$(echo "$CHANGES" | grep -E '^[+\-]' | grep -vE '^\+\+\+|^\-\-\-')
 
-# ignore blank lines
-CHANGES=$(echo "$CHANGES" | grep -vE '^[\+\-]\s*$')
+    # ignore blank lines
+    CHANGES=$(echo "$CHANGES" | grep -vE '^[\+\-]\s*$')
 
-# count total no of lines
-CHANGES=$(echo "$CHANGES" | wc -l)
+    # count total no of lines
+    CHANGES=$(echo "$CHANGES" | wc -l)
 
-echo "CHANGES MADE: $CHANGES"
+    echo "CHANGES MADE: $CHANGES"
 
-DELETE_LABELS=("v: minimal" "v: small" "v: moderate" "v: large")
+    DELETE_LABELS=("v: minimal" "v: small" "v: moderate" "v: large")
 
-if [ "$CHANGES" -gt "$SMALL_THRESHOLD" ]; then
-    LABEL="v: small"
-elif [ "$CHANGES" -gt "$MODERATE_THRESHOLD" ]; then
-    LABEL="v: moderate"
-elif [ "$CHANGES" -gt "$LARGE_THRESHOLD" ]; then
-    LABEL="v: large"
-else
-    LABEL="v: minimal"
-fi
+    if [ "$CHANGES" -gt "$SMALL_THRESHOLD" ]; then
+        LABEL="v: small"
+    elif [ "$CHANGES" -gt "$MODERATE_THRESHOLD" ]; then
+        LABEL="v: moderate"
+    elif [ "$CHANGES" -gt "$LARGE_THRESHOLD" ]; then
+        LABEL="v: large"
+    else
+        LABEL="v: minimal"
+    fi
 
-DELETE_LABELS=("${DELETE_LABELS[@]//${LABEL}/}")
+    DELETE_LABELS=("${DELETE_LABELS[@]//${LABEL}/}")
 
-echo "Adding label: $LABEL"
-curl -X POST \
-    -H "$AUTH_HEADER" \
-    -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    -d "{\"labels\":[\"$LABEL\"]}" \
-    "$API_URL" >/dev/null
-
-# delete labels other than the one corresponding to the no of changes made
-echo "Deleting Labels:"
-for DELETE_LABEL in "${DELETE_LABELS[@]}"; do
-    ENCODED_LABEL=$(echo "$DELETE_LABEL" | sed 's/ /%20/g')
-    curl -X DELETE \
-        -H "Accept: application/vnd.github+json" \
+    echo "Adding label: $LABEL"
+    curl -X POST \
         -H "$AUTH_HEADER" \
+        -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        "$API_URL/$ENCODED_LABEL" >/dev/null
-done
+        -d "{\"labels\":[\"$LABEL\"]}" \
+        "$API_URL" >/dev/null
+
+    # delete labels other than the one corresponding to the no of changes made
+    echo "Deleting Labels:"
+    for DELETE_LABEL in "${DELETE_LABELS[@]}"; do
+        ENCODED_LABEL=$(echo "$DELETE_LABEL" | sed 's/ /%20/g')
+        curl -X DELETE \
+            -H "Accept: application/vnd.github+json" \
+            -H "$AUTH_HEADER" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            "$API_URL/$ENCODED_LABEL" >/dev/null
+    done
+}
+
+size_label "$1" "$2"
