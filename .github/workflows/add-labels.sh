94e3b0ffe7d260eb02c7c2ff925d8d21e41665b2
adrinospy <amanmoon099@gmail.com>
2024-03-14 01:50:40 +0530

    made changes

--- .github/workflows/add-labels.sh
+++ .github/workflows/add-labels.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+# to run on pr-labeler.yml is called
+# adds labels based on size of changes in the pull request
+# varible accessed:
+#   CHANGES=$(git diff --ignore-all-space ${{ github.event.pull_request.base.sha }} ${{ github.event.pull_request.head.sha }})
+#   API_URL="https://api.github.com/repos/${{ github.repository }}/issues/${{ github.event.number }}/labels"
+#   AUTH_HEADER="Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}"
+#   GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+#   SMALL_THRESHOLD: ${{vars.SMALL_THRESHOLD}}
+#   MODERATE_THRESHOLD: ${{vars.MODERATE_THRESHOLD}}
+#   LARGE_THRESHOLD: ${{vars.LARGE_THRESHOLD}}
 
-# get only changes in git diff
+
+
+# get all the changes from git diff
 CHANGES=$(echo "$CHANGES" | grep -E '^[+\-]' | grep -vE '^\+\+\+|^\-\-\-')
 
 # ignore blank lines
@@ -12,6 +24,7 @@ CHANGES=$(echo "$CHANGES" | wc -l)
 echo "CHANGES MADE: $CHANGES"
 
 DELETE_LABELS=("v: minimal" "v: small" "v: moderate" "v: large")
+
 if [ "$CHANGES" -gt "$SMALL_THRESHOLD" ]; then
     LABEL="v: small"
 elif [ "$CHANGES" -gt "$MODERATE_THRESHOLD" ]; then
@@ -32,6 +45,7 @@ curl -X POST \
     -d "{\"labels\":[\"$LABEL\"]}" \
     "$API_URL" >/dev/null
 
+# delete labels other than the one corresponding to the no of changes made
 echo "Deleting Labels:"
 for DELETE_LABEL in "${DELETE_LABELS[@]}"; do
     ENCODED_LABEL=$(echo "$DELETE_LABEL" | sed 's/ /%20/g')
